26e8056cb7757ab5f61d9cdf5758699800d718a3
Kanupriya Agrawal <98919191+Kanupriya94@users.noreply.github.com>
2022-10-04 14:13:42 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Hello from CMPE281 assignment 1!
+          Hello from CMPE281 Lab 2 - Google Application Engine!
         </p>
       </header>
     </div>
